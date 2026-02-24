215b402139d6c6c5cc189088846c6a3293138f6a
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-02-24 19:46:59 +0700

    fix: add assertion in EshopApplicationTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,13 +2,15 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class EshopApplicationTests {
 
     @Test
     void main_shouldRunWithNonWebApplicationType() {
-        EshopApplication.main(new String[] {
+        assertDoesNotThrow(() -> EshopApplication.main(new String[] {
                 "--spring.main.web-application-type=none",
                 "--spring.main.banner-mode=off"
-        });
+        }));
     }
 }
