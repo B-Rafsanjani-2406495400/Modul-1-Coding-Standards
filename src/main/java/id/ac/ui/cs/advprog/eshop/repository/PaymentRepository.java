826726f038b18a15e15baa21b36a30a9557b7aa0
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-03-10 20:03:02 +0700

    [GREEN] Implement PaymentRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.List;
+
+public class PaymentRepository {
+
+    public Payment save(Payment payment) {
+        return null;
+    }
+
+    public Payment findById(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return null;
+    }
+}
